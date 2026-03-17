ff3418be7bf062a3eeaaef13c7a2917b049347df
shubham kondewar <shubham.kondewar98@gmail.com>
2026-03-17 13:09:06 +0530

    requirments update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Shubham',
+    author_email= 'shubhamkondewar98@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
